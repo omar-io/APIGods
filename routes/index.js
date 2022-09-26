510a668db00feebbad6f63e16a4e564de45a9988
Omar Ontiveros <omar.ontiveros@alumnos.udg.mx>
2022-09-25 22:42:08 -0500

    Main router fixed

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 
 // const gods = require('express').Router();
 // const realms = require('express').Router();
-const router = require('express').Router();
+const rootRouter = require('express').Router();
 
-router.get('/', (req, res) => {
+const gods = require('./gods');
+const realms = require('./realms');
+
+
+rootRouter.get('/', (req, res) => {
     res.json({'info': 'Welcome to gods API!'})
 })
+
+rootRouter.use('/gods', gods);
+rootRouter.use('/realms', realms)
 
 // router.use('/gods', gods);
 // router.use('/realms',realms);
 
-module.exports = router;
+module.exports = rootRouter;
